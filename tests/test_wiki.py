32aa1107420f1f6a2eb75f4c8ab82ffef26adcd1
D-Mariann <119792440+D-Mariann@users.noreply.github.com>
2024-09-18 19:01:38 +0300

    Update test_wiki.py

--- tests/test_wiki.py
+++ tests/test_wiki.py
@@ -1,15 +1,9 @@
-import time
 import chromedriver_autoinstaller
 import pytest
-from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from pages.the_search_phrase import *
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.common.exceptions import NoSuchElementException, InvalidSelectorException
-
-
 
 
 @pytest.fixture
@@ -22,11 +16,9 @@ def driver():
     return driver
 
 
-
 def test_search_wiki(driver):
-
     driver.get("https://ru.wikipedia.org/")
-    wiki_search = driver.find_element(By.ID ,"searchInput")
+    wiki_search = driver.find_element(By.ID, "searchInput")
     wiki_search.click()
     wiki_search.clear()
     wiki_search.send_keys('колобок')
@@ -41,12 +33,6 @@ def test_search_wiki(driver):
     WebDriverWait(driver, 10).until(
         lambda driver: driver.execute_script("return document.readyState") == "complete")
 
-
     assert 'журнал' in driver.title
-    # # title_word = driver.find_element(By.XPATH,'//*[@class="mw-page-title-main"and contains(text(), "журнал")]')
-    # title_word = driver.find_element(By.XPATH, '//*[@class="mw-page-title-main"]')
-    # text_title = driver.execute_script("return arguments[0].textContent",title_word)
-    # # title_word.get_attribute('textContent'))
-    # print(text_title)
-    # print(ti)
+
     driver.quit()
